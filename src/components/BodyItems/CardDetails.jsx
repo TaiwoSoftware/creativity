bdfc4b53e60563fd81159374a162404bdb40b071
Taiwo Akerele <davcodes24@gmail.com>
2024-05-21 00:41:57 -0700

    imported necessary icons

--- src/components/BodyItems/CardDetails.jsx
+++ src/components/BodyItems/CardDetails.jsx
@@ -1,3 +1,7 @@
+import { GiCargoShip } from "react-icons/gi";
+import { FiTruck } from "react-icons/fi";
+import { BsAirplane } from "react-icons/bs";
+import { LuShip } from "react-icons/lu";
 export default function CardDetails() {
   return (
     <div>
